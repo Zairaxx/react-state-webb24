f866ae89095460bb0accac7260b6b01d7ed43621
Zairaxx <brandontsegai@gmail.com>
2024-11-08 10:12:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [showSlogan, setShowSlogan] = useState(false);
+  return (
+    <>
+    <h1>React state - Webb24</h1>
+    <button onClick={() => setShowSlogan(!showSlogan)}>Toggle slogan</button>
+
+    {/* W ? T : F */}
+    {/* {showSlogan ? <h2>Anyone can code!</h2> : <h2>Vi har inget motto..</h2>} */}
+    {showSlogan && <h2>Anyone can code!</h2>}
+    </>
+  )
+}
+
+export default App
+
+
+let addTwo = x => x+2
